08db0770d68984a863bfb014878e349bad6444be
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-02-28 17:03:07 +0800

    add: tailwind config for LSP

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+// DO NOT EDIT!!
+// Used for tailwind LSP in Neovim only.
+module.exports = {
+    content: [ "./web/templates/**/*.{go,js,templ,html}"],
+    theme: { extend: {}, },
+    plugins: [],
+}

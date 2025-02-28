dc19a5b10d9339691f0db07b49faa6176a5e353f
Knu09 <christiandelatorre1985@gmail.com>
2025-03-01 02:34:31 +0800

    change anchor source

--- web/templates/components/header_templ.go
+++ web/templates/components/header_templ.go
@@ -29,7 +29,7 @@ func Header() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<nav class=\"bg-[#252525] text-white\"><div class=\"flex flex-wrap items-center justify-between mx-auto p-6\"><div class=\"flex gap-3\"><button type=\"button\" id=\"menuToggleBtn\" name=\"menu\" class=\"cursor-pointer\" @click=\"open = !open\"><span class=\"sr-only\">Open sidebar</span> <i class=\"fa-solid fa-bars\"></i></button><h3 class=\"font-medium\">Budget and Expense</h3></div><div><a href=\"/signup\">Sign up</a></div></div></nav>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<nav class=\"bg-[#252525] text-white\"><div class=\"flex flex-wrap items-center justify-between mx-auto p-6\"><div class=\"flex gap-3\"><button type=\"button\" id=\"menuToggleBtn\" name=\"menu\" class=\"cursor-pointer\" @click=\"open = !open\"><span class=\"sr-only\">Open sidebar</span> <i class=\"fa-solid fa-bars\"></i></button><h3 class=\"font-medium\">Budget and Expense</h3></div><div><a href=\"/register\">Sign up</a></div></div></nav>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
